cd6bfadcce0c1a3503c30aa934b94b4757f4711f
isaacluke <58997330+isaacluke@users.noreply.github.com>
2024-05-10 08:50:38 +0100

    sorts articles

--- src/components/topics/Topics.jsx
+++ src/components/topics/Topics.jsx
@@ -5,12 +5,29 @@ import './Topics.css'
 
 export default function () {
   const [topics, setTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
+    setIsLoading(true)
     getTopics().then((topicsData) => {
       setTopics(topicsData.data.topics);
+      setIsLoading(false)
+    }).catch(()=>{
+        setIsError(true)
     });
   }, []);
+
+  if (isError) {
+    return <h2>Whoopsie! Something went wrong</h2>;
+  }
+  
+  if (isLoading) {
+    return <h2>Hang tight whilst we load your page {`:)`}</h2>;
+  }
+
+
+
   return (
     <>
       {topics.map((topic) => {
